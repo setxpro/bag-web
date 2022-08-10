a777f2e48a5bdcd1ae2a01ac9e0d578897842bec
Patrick Anjos <patrickpqdt87289@gmail.com>
2022-08-10 08:37:29 -0300

    define-scrollbar

--- src/Styles/GlobalStyles.ts
+++ src/Styles/GlobalStyles.ts
@@ -1,13 +1,30 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+
+    /** 
+    * Estilos gerais da aplicação
+    */
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
     }
     body {
-        font-size: calc(60% + .8vmin);
+        font-size: calc(60% + .8vmin); /** Faz com que o texto seja Responsivo */
         font-family: 'Montserrat', sans-serif;
+    }
+
+    /**
+    *   Estilos da Scrollbar
+    */
+    ::-webkit-scrollbar {
+        width: 4px;
+    }
+    ::-webkit-scrollbar-track {
+        background: rgba(0, 0, 0, 0.2);
+    }
+    ::-webkit-scrollbar-thumb {
+        background: rgba(0, 0, 0, 0.6);
     }
 `;
